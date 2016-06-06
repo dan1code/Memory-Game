ca6989641ac38ef961da3f5a0ff4aed08a8d73bb
dan1code <daniel_ayre1990@hotmail.com>
2016-06-06 10:53:50 +0000

    Add files via upload

--- Memory-Game/flip.js
+++ Memory-Game/flip.js
@@ -1,9 +1,15 @@
 $( document ).ready(function() {
 
+var counter = 0;
+
   $('.flip').click(function(){
-    $(this).find('.card').addClass('flipped').mouseleave(function(){
-        $(this).removeClass('flipped');
-    });
-    return false;
+     $(this).find('.card').addClass('flipped');
+	 counter++;
+		if (counter == 2){
+			setTimeout(function() {$('.card').removeClass('flipped')}, 1500);
+			counter = 0;
+		}
+	});
 });
-});
+
+
